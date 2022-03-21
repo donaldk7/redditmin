601fb70fb6593e505dfdbf02ec9a4108ed83cd49
donaldk7 <donaldkim731@gmail.com>
2022-03-21 13:54:17 -0700

    added initial files

---
+++ src/components/navigation/index.js
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+
+
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Article</li>
+                <li>Search</li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Navigation;
---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+
+
+const HomePage = () => {
+
+
+    return (
+        <div className="page">
+            <h1>Home Page</h1>
+            <h2>Article</h2>
+            <img alt="an image"/>
+            <p>some article</p>
+        </div>
+    )
+}
+
+export default HomePage;
